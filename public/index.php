0603e28a3a48b9127321fb4c46b7979d46d4344e
evgen <don.evgen86@gmail.com>
2023-02-15 17:17:54 +0300

    new commit nginx

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    Это уже php-шная страница на nginx'е (Крутяк)
+</body>
+</html>
